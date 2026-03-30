59a1b0fc95f2a98c7fea1cfc23eb5c45787f83f9
stivan301 <spapanov@gmail.com>
2026-03-30 10:13:39 +0300

    Create cars method

---
+++ WebAppRentAcar/ViewModels/CarsCreateViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAppRentAcar.ViewModels
+{
+    public class CarsCreateViewModel
+    {
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Brand { get; set; } = null!;
+
+        [Required]
+        [StringLength(50, MinimumLength = 2)]
+        public string Model { get; set; } = null!;
+
+        [Required]
+        [Range(1950, 2080)]
+        public int Year { get; set; }
+
+        [Required]
+        [Range(1, 6)]
+        public int Passengers { get; set; }
+
+        [MaxLength(4048)]
+        public string? Description { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "1", "500")]
+        public decimal PricePerDay { get; set; }
+    }
+}
