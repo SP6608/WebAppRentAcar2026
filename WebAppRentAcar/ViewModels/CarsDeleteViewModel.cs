f2b42dd3a27076cc3efd75a6f8819e5058b481c2
SP6608 <spapanov@gmail.com>
2026-03-31 22:07:22 +0300

    Създавам View модел за Delete Car

---
+++ WebAppRentAcar/ViewModels/CarsDeleteViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebAppRentAcar.ViewModels
+{
+    public class CarsDeleteViewModel
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; } = null!;
+        public string Model { get; set; } = null!;
+        public int Year { get; set; }
+        public int Passengers { get; set; }
+        public decimal PricePerDay { get; set; }
+        public string? Description { get; set; }
+    }
+}
